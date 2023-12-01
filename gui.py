8a75456394926b95e39c0c2c95fbffce6eedfc60
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-12-01 13:45:08 -1000

    Remove preset dropdown, improve messaging on error and cancel

--- gui.py
+++ gui.py
@@ -12,7 +12,6 @@ class Gui(tk.Tk):
         self.minsize(350, 400)
 
         self.cruise_number = tk.StringVar(value='')
-        self.cruise_preset = tk.StringVar(value='EX')
         self.source_folder = tk.StringVar(value='/Volumes/maxarray2/varsadditional')
         self.output_folder = tk.StringVar(value='/Volumes/maxarray2/varsadditional')
 
@@ -29,14 +28,14 @@ class Gui(tk.Tk):
         source_folder = self.source_folder.get()
         output_folder = self.output_folder.get()
         process = None
-        if self.cruise_preset.get() == 'EX':
+        if cruise_number.startswith('EX'):
             process = subprocess.Popen([
                 'EX/ex_ctd_processor.sh',
                 cruise_number,
                 source_folder,
                 output_folder,
             ])
-        elif self.cruise_preset.get() == 'NA':
+        elif cruise_number.startswith('NA'):
             process = subprocess.Popen([
                 'NA/na_ctd_processor.sh',
                 cruise_number,
@@ -44,12 +43,15 @@ class Gui(tk.Tk):
                 f'{source_folder}/processed/dive_reports',
                 output_folder,
             ])
+        else:
+            self.processing_text.set('Could not determine preset \nCruise number should start with "NA" or "EX"')
+            return
         self.button_text.set('CANCEL')
         go_button.config(command=lambda: self.stop_button_callback(process, go_button))
-        self.processing_text.set('Processing... see details in terminal')
+        self.processing_text.set('Processing - See terminal for details')
         self.processing = True
         self.update_spinner(0)
-        self.check_process(process)
+        self.check_process(process, go_button)
 
     def stop_button_callback(self, process, go_button):
         process.terminate()
@@ -58,16 +60,18 @@ class Gui(tk.Tk):
         go_button.config(command=lambda: self.go_button_callback(go_button))
         self.processing_text.set('Cancelled')
 
-    def check_process(self, process):
+    def check_process(self, process, go_button):
         if process.poll() is None:
-            self.after(1000, self.check_process, process)
+            self.after(1000, self.check_process, process, go_button)
         else:
+            exit_val = process.returncode
             self.processing = False
             self.button_text.set('GO')
+            go_button.config(command=lambda: self.go_button_callback(go_button))
+            if exit_val != 0:
+                self.processing_text.set('Cancelled' if self.processing_text.get() == 'Cancelled' else 'Error - See terminal for details')
+                return
             self.processing_text.set('Cancelled' if self.processing_text.get() == 'Cancelled' else 'Complete!')
-
-    def update_preset(self, preset):
-        self.cruise_preset.set(preset)
 
     def get_file_path(self, path):
         file_path = filedialog.askdirectory(initialdir='/Volumes/maxarray2/varsadditional', title='Select a folder')
@@ -85,24 +89,14 @@ class Gui(tk.Tk):
     def initialize_widgets(self):
         background = self.background
 
-        # cruise and preset frame
-        cruise_preset_frame = tk.Frame(master=background)
-
         # cruise number input
-        cruise_frame = tk.Frame(master=cruise_preset_frame)
+        cruise_frame = tk.Frame(master=background)
         cruise_label = tk.Label(master=cruise_frame, text='CRUISE NUMBER', font=('Helvetica', '13', 'bold'))
         cruise_entry = tk.Entry(
             master=cruise_frame,
-            width=15,
+            width=30,
             textvariable=self.cruise_number,
         )
-
-        # preset input
-        preset_frame = tk.Frame(master=cruise_preset_frame)
-        preset_label = tk.Label(master=preset_frame, text='PRESET', font=('Helvetica', '13', 'bold'))
-        preset_combobox = ttk.Combobox(master=preset_frame, values=['EX', 'NA'], state='readonly', width=10)
-        preset_combobox.current(0)
-        preset_combobox.bind('<<ComboboxSelected>>', lambda event: self.update_preset(preset_combobox.get()))
 
         # base directory input
         base_directory_frame = tk.Frame(master=background)
@@ -158,15 +152,9 @@ class Gui(tk.Tk):
         )
 
         # packin
-        cruise_preset_frame.pack(pady=(5, 10), fill=tk.X)
-
-        cruise_frame.pack(side=tk.LEFT)
+        cruise_frame.pack(pady=(5, 10), fill=tk.X)
         cruise_label.pack(side=tk.TOP, anchor='w')
         cruise_entry.pack(side=tk.TOP, anchor='w')
-
-        preset_frame.pack(side=tk.RIGHT, expand=True, fill=tk.X, padx=20)
-        preset_label.pack(side=tk.TOP, anchor='w', pady=(0, 2))
-        preset_combobox.pack(side=tk.TOP, anchor='w')
 
         base_directory_frame.pack(pady=10, fill=tk.X)
         base_directory_label.pack(anchor='w')
